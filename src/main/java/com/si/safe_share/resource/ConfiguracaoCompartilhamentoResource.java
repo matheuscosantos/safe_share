59f145fea402109a106abfced068e61b0a5478f8
Matheus C O Santos <oliveiramatheus77@gmail.com>
2020-10-21 21:40:32 -0300

    Adiciona log com LogBack

--- src/main/java/com/si/safe_share/resource/ConfiguracaoCompartilhamentoResource.java
+++ src/main/java/com/si/safe_share/resource/ConfiguracaoCompartilhamentoResource.java
@@ -7,6 +7,7 @@ import com.si.safe_share.repository.ClienteRepository;
 import com.si.safe_share.repository.ConfiguracaoCompartilhamentoRepository;
 import com.si.safe_share.repository.ContainerDeChavesRepository;
 import com.si.safe_share.resource.form.ConfiguracaoCompartilhamentoForm;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -26,9 +27,14 @@ import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+
 @RestController
 @RequestMapping(value = "/api")
 public class ConfiguracaoCompartilhamentoResource {
+
+    Logger logger = LoggerFactory.getLogger(ConfiguracaoCompartilhamentoResource.class);
+
     @Autowired
     ConfiguracaoCompartilhamentoRepository configuracaoCompartilhamentoRepository;
     
@@ -79,7 +85,6 @@ public class ConfiguracaoCompartilhamentoResource {
             IvParameterSpec ivparameterspec = new IvParameterSpec(key);
             cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivparameterspec);
 
-            byte[] email = cipher.doFinal(cliente.get().getEmail().getBytes("UTF8"));
             byte[] endereco = cipher.doFinal(cliente.get().getEndereco().getBytes("UTF8"));
             byte[] telefone = cipher.doFinal(cliente.get().getTelefone().getBytes("UTF8"));
             byte[] nome = cipher.doFinal(cliente.get().getNome().getBytes("UTF8"));
@@ -88,14 +93,12 @@ public class ConfiguracaoCompartilhamentoResource {
 
             Base64.Encoder encoder = Base64.getEncoder();
 
-            String emailCriptografado = encoder.encodeToString(email);
             String enderecoCriptografado = encoder.encodeToString(endereco);
             String telefoneCriptografado = encoder.encodeToString(telefone);
             String nomeCriptografado = encoder.encodeToString(nome);
             String sobrenomeCriptografado = encoder.encodeToString(sobrenome);
             String cpfCriptografado = encoder.encodeToString(cpf);
 
-            cliente.get().setEmail(emailCriptografado);
             cliente.get().setCpf(cpfCriptografado);
             cliente.get().setNome(nomeCriptografado);
             cliente.get().setSobrenome(sobrenomeCriptografado);
@@ -103,6 +106,7 @@ public class ConfiguracaoCompartilhamentoResource {
             cliente.get().setEndereco(enderecoCriptografado);
 
             clienteRepository.save(cliente.get());
+            logger.info("Dados do usuário "+cliente.get().getEmail()+" ofuscados");
 
         }else if(compartilhamentoForm.getCompartilha_dados_pessoais() == true){
             Cipher cipher = Cipher.getInstance(cipherTransformation);
@@ -112,21 +116,18 @@ public class ConfiguracaoCompartilhamentoResource {
             cipher.init(Cipher.DECRYPT_MODE, secretKey, ivparameterspec);
             Base64.Decoder decoder = Base64.getDecoder();
 
-            byte[] emailCriptografado = decoder.decode(cliente.get().getEmail().getBytes("UTF8"));
             byte[] cpfCriptografado = decoder.decode(cliente.get().getCpf().getBytes("UTF8"));
             byte[] nomeCriptografado = decoder.decode(cliente.get().getNome().getBytes("UTF8"));
             byte[] sobrenomeCriptografado = decoder.decode(cliente.get().getSobrenome().getBytes("UTF8"));
             byte[] telefoneCriptografado = decoder.decode(cliente.get().getTelefone().getBytes("UTF8"));
             byte[] enderecoCriptografado = decoder.decode(cliente.get().getEndereco().getBytes("UTF8"));
 
-            String emailDecriptado = new String(cipher.doFinal(emailCriptografado), "UTF-8");
             String cpfDecriptado = new String(cipher.doFinal(cpfCriptografado), "UTF-8");
             String nomeDecriptado = new String(cipher.doFinal(nomeCriptografado), "UTF-8");
             String sobrenomeDecriptado = new String(cipher.doFinal(sobrenomeCriptografado), "UTF-8");
             String telefoneDecriptado = new String(cipher.doFinal(telefoneCriptografado), "UTF-8");
             String enderecoDecriptado = new String(cipher.doFinal(enderecoCriptografado), "UTF-8");
 
-            cliente.get().setEmail(emailDecriptado);
             cliente.get().setCpf(cpfDecriptado);
             cliente.get().setNome(nomeDecriptado);
             cliente.get().setSobrenome(sobrenomeDecriptado);
@@ -134,8 +135,8 @@ public class ConfiguracaoCompartilhamentoResource {
             cliente.get().setEndereco(enderecoDecriptado);
 
             clienteRepository.save(cliente.get());
+            logger.info("Dados do usuário "+cliente.get().getEmail()+" desofuscados");
         }
-
         return configuracaoCompartilhamentoRepository.save(configuracaoCompartilhamento);
     }
 
